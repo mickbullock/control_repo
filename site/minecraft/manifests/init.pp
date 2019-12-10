32d84ca7bf859c1bf8a6e9c4ffe073937293164b
mickbullock <mickbullock@live.com>
2019-12-10 15:15:37 +1100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,7 +19,9 @@ $install_dir = '/opt/minecraft'
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    content => epp('minecraft/minecraft.service',{
+      installdir => $install_dir
+    })
   }
   service {'minecraft':
     ensure => running,
